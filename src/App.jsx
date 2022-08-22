62ed7a13f9f377a3cc0c181e3d3eee2b5877c9d8
ItVal <valentinhonvj1@gmail.com>
2022-08-22 13:47:57 +0200

    refact : resolve conflit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import TodoList from './Components/Todolist'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <TodoList />
+     
     </div>
   )
 }
